32efe19e0dd4c25f6736954aaeac3d43b0807bed
fakerr <111339712+not-first@users.noreply.github.com>
2024-07-12 14:52:06 +1000

    add session history command to view past sessions with date, length and goal in list item accessories

--- src/session-history.tsx
+++ src/session-history.tsx
@@ -1,4 +1,4 @@
-import { getPreferenceValues, List } from "@raycast/api";
+import { getPreferenceValues, Icon, List } from "@raycast/api";
 import { showFailureToast, useCachedPromise } from "@raycast/utils";
 import fetch from "node-fetch";
 
@@ -17,31 +17,56 @@ interface Session {
 }
 
 export default function SessionHistory() {
-  const { isLoading, data } = useCachedPromise(async () => {
-    try {
-      const response = await fetch(`https://hackhour.hackclub.com/api/history/${getPreferenceValues().userid}`, {
-        method: "GET",
-        headers: {
-          "Content-Type": "application/json",
-          Authorization: `Bearer ${getPreferenceValues().apiToken}`,
-        },
-      });
+  const { isLoading, data } = useCachedPromise(
+    async () => {
+      try {
+        const response = await fetch(`https://hackhour.hackclub.com/api/history/${getPreferenceValues().userid}`, {
+          method: "GET",
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${getPreferenceValues().apiToken}`,
+          },
+        });
 
-      if (!response.ok) {
-        showFailureToast(response.statusText, { title: "Failed to fetch session history" });
+        if (!response.ok) {
+          showFailureToast(response.statusText, { title: "Failed to fetch session history" });
+        }
+
+        const data = (await response.json()) as SessionHistoryResponse;
+        const sortedData: Session[] = data?.data.sort(
+          (a, b) => new Date(b.createdAt).getTime() - new Date(a.createdAt).getTime()
+        );
+
+        return sortedData;
+      } catch (error) {
+        console.error("An error occurred", error);
       }
-
-      const data = (await response.json()) as SessionHistoryResponse;
-      return data.data;
-    } catch (error) {
-      console.error("An error occurred", error);
-    }
-  });
+    },
+    [],
+    { keepPreviousData: true }
+  );
 
   return (
-    <List filtering={false} isLoading={isLoading}>
+    <List filtering={false} isLoading={isLoading} searchBarPlaceholder="">
       {data?.map((session: Session) => {
-        return <List.Item key={session.createdAt.toString()} title={session.work} />;
+        return (
+          <List.Item
+            key={session.createdAt.toString()}
+            title={session.work.length > 75 ? `${session.work.substring(0, 75)}...` : session.work}
+            accessories={[
+              { text: session.goal, tooltip: "Session goal"},
+              ...(session.ended
+                ? [
+                    {
+                      tag: { value: session.elapsed.toString() + "m" },
+                      tooltip: `Session length`,
+                    },
+                  ]
+                : [{ icon: Icon.Clock, tooltip: "Ongoing Session"}]),
+              { date: new Date(session.createdAt), tooltip: "Session starting time"},
+            ]}
+          />
+        );
       })}
     </List>
   );
