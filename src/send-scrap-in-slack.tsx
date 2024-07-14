dd354839229c2d556ba978f61aeb5d2445b932aa
fakerr <111339712+not-first@users.noreply.github.com>
2024-07-14 11:29:51 +1000

    slack scrap sender with file attachments working.

--- src/send-scrap-in-slack.tsx
+++ src/send-scrap-in-slack.tsx
@@ -3,6 +3,7 @@ import fetch from "node-fetch";
 import path from "path";
 import { FormValidation, showFailureToast, useCachedPromise, useForm } from "@raycast/utils";
 import { getCurrentSession, useSlack } from "./api";
+import { MessageAttachment } from "@slack/web-api";
 
 interface FormValues {
   text: string;
@@ -26,47 +27,45 @@ export default function SendScrapInSlack() {
           return;
         }
 
-        const filesUrls: string[] = [];
+        const attachments: MessageAttachment[] = [];
         if (values.file !== undefined && values.file.length > 0) {
           const toast = await showToast({
             style: Toast.Style.Animated,
             title: "Uploading files",
           });
-          for (const file of values.file) {
-            const filename = path.basename(file);
-            const response = await slack?.files.uploadV2({
-              file: file,
-              filename: filename,
-            });
 
-            if (!response.ok) {
-              await showFailureToast(response.error, { title: "Failed to upload file" });
-            }
+          const filesToUpload = values.file.map((file) => ({
+            file: file,
+            filename: path.basename(file),
+          }));
 
-            filesUrls.push(response["files"][0]["files"][0]["permalink"]);
+          const response = await slack.filesUploadV2({
+            channel_id: "C06SBHMQU8G",
+            thread_ts: messageTs,
+            initial_comment: values.text,
+            file_uploads: filesToUpload,
+          });
+
+          if (!response.ok) {
+            await showFailureToast(response.error, { title: "Failed to upload file" });
+            return
           }
-          console.log(JSON.stringify(filesUrls));
+
           toast.style = Toast.Style.Success;
           toast.message = "Uploaded files";
+          
+        } else {
+          console.log("attachments" + JSON.stringify(attachments));
+          const response = await slack?.chat.postMessage({
+            text: values.text,
+            channel: "C06SBHMQU8G",
+            thread_ts: messageTs,
+          });
+          if (!response.ok) {
+            await showFailureToast(response.error, { title: "Failed to pause session" });
+          }
         }
 
-        const attachments =
-          filesUrls.length > 0
-            ? filesUrls.map((url) => ({image_url: url }))
-            : [];
-        // const blocks = [{type: "image", alt_text: "uploaded image", "image_url": "https://private-user-images.githubusercontent.com/111339712/348129959-9102d3c4-5136-4870-8238-3549bb10702a.jpeg?jwt=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJnaXRodWIuY29tIiwiYXVkIjoicmF3LmdpdGh1YnVzZXJjb250ZW50LmNvbSIsImtleSI6ImtleTUiLCJleHAiOjE3MjA4Nzg0OTAsIm5iZiI6MTcyMDg3ODE5MCwicGF0aCI6Ii8xMTEzMzk3MTIvMzQ4MTI5OTU5LTkxMDJkM2M0LTUxMzYtNDg3MC04MjM4LTM1NDliYjEwNzAyYS5qcGVnP1gtQW16LUFsZ29yaXRobT1BV1M0LUhNQUMtU0hBMjU2JlgtQW16LUNyZWRlbnRpYWw9QUtJQVZDT0RZTFNBNTNQUUs0WkElMkYyMDI0MDcxMyUyRnVzLWVhc3QtMSUyRnMzJTJGYXdzNF9yZXF1ZXN0JlgtQW16LURhdGU9MjAyNDA3MTNUMTM0MzEwWiZYLUFtei1FeHBpcmVzPTMwMCZYLUFtei1TaWduYXR1cmU9YmJhMTUzMTk5OGJlYWI2OTk0M2U5NTg5MWIzMTk1NGE4YzZkMzQ5ZGZjZTRhOGJjMmYwZTVmOGQ4NzBmY2IzOCZYLUFtei1TaWduZWRIZWFkZXJzPWhvc3QmYWN0b3JfaWQ9MCZrZXlfaWQ9MCZyZXBvX2lkPTAifQ.7yJXGozG3m2ievwNP4WTyOAALyXsDCDepU1cbVK0ANM"}];
-        console.log(JSON.stringify(attachments));
-        const response = await slack?.chat.postMessage({
-          text: values.text,
-          channel: "C06SBHMQU8G",
-          thread_ts: messageTs,
-          attachments: attachments,
-        });
-        if (!response.ok) {
-          await showFailureToast(response.error, { title: "Failed to pause session" });
-        }
-
-        console.log(JSON.stringify(response));
         await showHUD("Scrap sent successfully!", { popToRootType: PopToRootType.Immediate });
       } catch (error) {
         console.error("An error occurred", error);
@@ -85,7 +84,7 @@ export default function SendScrapInSlack() {
         </ActionPanel>
       }
     >
-      <Form.TextField
+      <Form.TextArea
         title="Text"
         info="The text content of the slack message"
         placeholder="I am working on..."
